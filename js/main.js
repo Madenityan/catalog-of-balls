c5de7f03351447c6e0f07082a490c4041732ac59
unknown <marinakorsyn@gmail.com>
2018-07-04 17:48:56 +0300

    added a catalog of balls

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+});
+
+$(".plus-btn").on("click",function(){
+    var $input = $(this).parent().parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
+$('.minus-btn').on("click",function(){
+    var $input = $(this).parent().parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
